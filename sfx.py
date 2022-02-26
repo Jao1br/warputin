1652ce1064cabe8ce59d38cb2776c9c6f001ae3e
Jao1br <100004933+Jao1br@users.noreply.github.com>
2022-02-26 17:16:49 -0300

    comit
    war o

---
+++ sfx.py
@@ -0,0 +1,10 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+input=name('Qual seu nome?')
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    input=name('Qual seu nome?')
+NameError: name 'name' is not defined
+input=('Qual seu nome?')
+input=('Qual sua idade')
+input=('Qual seu link no Github?')
